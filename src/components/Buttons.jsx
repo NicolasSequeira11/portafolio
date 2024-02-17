1d2f7858f4a4c7742783f530239b2eb45b9d514c
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2024-02-17 01:15:58 -0300

    download pdf

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
+import DownloadIcon from '@mui/icons-material/Download';
 
 export const PrincipalButton = ({text, background, textColor, shadow, onClick}) => {
   return(
@@ -13,3 +14,23 @@ export const PrincipalButton = ({text, background, textColor, shadow, onClick}) 
     </>
   )
 }
+
+ export const PdfDownloadButton = ({ pdfUrl, pdfName, extraClass }) => {
+  const handleDownload = () => {
+    // Crear un elemento <a> temporal
+    const downloadLink = document.createElement('a');
+    downloadLink.href = pdfUrl;
+    downloadLink.download = pdfName;
+
+    // Simular clic en el elemento temporal para iniciar la descarga
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  return (
+    <div className={`w-fit mx-auto p-3 rounded-full cursor-pointer ${extraClass}`}>
+      <DownloadIcon onClick={handleDownload}>Descargar PDF</DownloadIcon>
+    </div>
+  );
+};
