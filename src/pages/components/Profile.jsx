c5f3467698c7e061d3ae0050a13e65e0f5a05288
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2024-02-17 00:10:32 -0300

    fixed

--- src/pages/components/Profile.jsx
+++ src/pages/components/Profile.jsx
@@ -12,7 +12,7 @@ export const Profile = ({ language, darkmode}) => {
       }
     >
       <p 
-        className="font-bold text-6xl mx-10 mt-36 max-sm:text-5xl text-center"
+        className="font-bold text-6xl mt-28 max-sm:text-5xl text-center"
       >
         {language ? "Desarrollador Frontend" : "Frontend Developer"}
       </p>
