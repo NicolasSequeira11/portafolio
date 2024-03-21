03842bf30487a9f06e203905c534732682ee3527
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2024-03-21 19:48:41 -0300

    update cv english

--- src/pages/components/Curriculum.jsx
+++ src/pages/components/Curriculum.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { PdfDownloadButton } from "../../components/Buttons";
 import CVEnglish from "/media/cv-english.png";
-import CVSpanish from "/media/cv-spanish.png";
 import CVEnglishPDF from "/media/cv-english-nicolas-sequeira-front-developer.pdf";
-import CVSpanishPDF from "/media/cv-nicolas-sequeira-front-developer.pdf";
 
 export const Curriculum = ({ language, darkmode }) => {
   return(
@@ -12,17 +10,14 @@ export const Curriculum = ({ language, darkmode }) => {
     }>
       <div className="m-auto text-center">
         <img 
-          src={language ? CVEnglish : CVSpanish} 
+          src={CVEnglish} 
           className="max-lg:w-11/12 lg:w-6/12 flex object-contain m-auto border-4 border-lightblue"
         />
         <div className="m-auto justify-center mt-12 w-fit">
           <PdfDownloadButton 
             extraClass="bg-gradient-to-tr to-lightblue via-blue-800 from-darkblue text-white shadow-md"
-            pdfUrl={language ? CVEnglishPDF : CVSpanishPDF} 
-            pdfName={language 
-              ? "cv-nicolas-sequeira-front-developer.pdf" 
-              : "cv-english-nicolas-sequeira-front-developer.pdf"
-            } 
+            pdfUrl={CVEnglishPDF} 
+            pdfName="cv-english-nicolas-sequeira-front-developer.pdf"
           />
         </div>
       </div>
